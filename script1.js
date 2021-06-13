6863dae782872da79f1f4fb5e594fc9ca9b33023
varnika9695 <83939370+varnika9695@users.noreply.github.com>
2021-06-13 11:35:09 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+    document.querySelector("div").innerHTML = 10;
+    setTimeout(() => {
+        document.querySelector("div").innerHTML = 9;
+        setTimeout(() => {
+            document.querySelector("div").innerHTML = 8;
+            setTimeout(() => {
+                document.querySelector("div").innerHTML = 7;
+                setTimeout(() => {
+                    document.querySelector("div").innerHTML = 6;
+                    setTimeout(() => {
+                        document.querySelector("div").innerHTML = 5;
+                        setTimeout(() => {
+                            document.querySelector("div").innerHTML = 4;
+                            setTimeout(() => {
+                                document.querySelector("div").innerHTML = 3;
+                                setTimeout(() => {
+                                    document.querySelector("div").innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.querySelector("div").innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.querySelector("div").innerHTML = 0;
+                                            setTimeout(() => {
+                                                document.querySelector("div").innerHTML = "Happy Independance Day";
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
